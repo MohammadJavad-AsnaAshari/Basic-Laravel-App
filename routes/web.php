1ffd0dfa1949226e579e1b35426770f091fcbd4a
MohammadJavad-AsnaAshari <mohammadjavadasnaashari@gmail.com>
2023-08-29 17:04:59 +0330

    add Blade

--- routes/web.php
+++ routes/web.php
@@ -18,15 +18,41 @@ Route::get('/', function () {
 });
 
 Route::get("/home", function () {
-    return "Home Page";
+    return view("home.index");
 })->name("home.index");
 
 Route::get("/contact", function () {
-    return "Contact Page";
+    return view("home.contact");
 })->name("home.contact");
 
-Route::get("/posts/{id}", function ($id) {
-    return "Blog post " . $id;
+$posts = [
+    1 => [
+        "title" => "Intro to Laravel",
+        "content" => "This is a short intro to Laravel",
+        "is_new" => true,
+        "has_comments" => true
+    ],
+    2 => [
+        "title" => "Intro to PHP",
+        "content" => "This is a short intro to PHP",
+        "is_new" => false,
+        "has_comments" => true
+    ],
+    3 => [
+        "title" => "Intro to SQL",
+        "content" => "This is a short intro to SQL",
+        "is_new" => false,
+        "has_comments" => false
+    ]
+];
+
+Route::get("/posts", function () use ($posts) {
+    return view("posts.index", compact("posts"));
+});
+
+Route::get("/posts/{id}", function ($id) use ($posts) {
+    abort_if(!isset($posts[$id]), 404);
+    return view("posts.show", ["post" => $posts[$id]]);
 })->name("posts.show");
 
 Route::get("/recent-posts/{days_ago?}", function ($daysAgo = 20) {
