d74ba2455d0354142efa0bac674da6b56f680f4a
MohammadJavad-AsnaAshari <mohammadjavadasnaashari@gmail.com>
2023-08-29 15:13:46 +0330

    add Routing

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/home", function () {
+    return "Home Page";
+})->name("home.index");
+
+Route::get("/contact", function () {
+    return "Contact Page";
+})->name("home.contact");
+
+Route::get("/posts/{id}", function ($id) {
+    return "Blog post " . $id;
+})->name("posts.show");
+
+Route::get("/recent-posts/{days_ago?}", function ($daysAgo = 20) {
+    return "Posts from " . $daysAgo . " days ago";
+})->name("posts.recent.index");
