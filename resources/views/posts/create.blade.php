66b7a74a0c145c76af04a101fa229e068e8c2cc1
MohammadJavad-AsnaAshari <mohammadjavadasnaashari@gmail.com>
2023-09-02 18:08:33 +0330

    add Displaying Validation Errors

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -5,12 +5,28 @@
 @section('content')
     <form action="{{route('posts.store')}}" method="Post">
         @csrf
+
         <div>
-            <input type="text" name="title">
+            <input type="text" name="title" value="{{old('title')}}">
+            @error("title")
+                <div>{{$message}}</div>
+            @enderror
         </div>
         <div>
             <textarea name="content"></textarea>
+            @error("content")
+                <div>{{$message}}</div>
+            @enderror
         </div>
+        @if($errors->any())
+            <div>
+                <ul>
+                    @foreach($errors->all() as $error)
+                        <li>{{$error}}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
         <div>
             <input type="submit" value="Create">
         </div>
