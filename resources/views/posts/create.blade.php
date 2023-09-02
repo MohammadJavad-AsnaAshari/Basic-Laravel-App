c773d7cb4de9e145402e25db60dcb673666a6e18
MohammadJavad-AsnaAshari <mohammadjavadasnaashari@gmail.com>
2023-09-02 20:12:47 +0330

    add Edit Blog Post

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -6,27 +6,7 @@
     <form action="{{route('posts.store')}}" method="Post">
         @csrf
 
-        <div>
-            <input type="text" name="title" value="{{old('title')}}">
-            @error("title")
-                <div>{{$message}}</div>
-            @enderror
-        </div>
-        <div>
-            <textarea name="content">{{old('content')}}</textarea>
-            @error("content")
-                <div>{{$message}}</div>
-            @enderror
-        </div>
-        @if($errors->any())
-            <div>
-                <ul>
-                    @foreach($errors->all() as $error)
-                        <li>{{$error}}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
+        @include("posts.partials.form")
         <div>
             <input type="submit" value="Create">
         </div>
---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section("title", "Update The Post")
+
+@section('content')
+    <form action="{{route('posts.update', ['post' => $post->id])}}" method="post">
+        @csrf
+        @method("put")
+
+        @include("posts.partials.form")
+        <div>
+            <input type="submit" value="Update">
+        </div>
+    </form>
+@endsection
