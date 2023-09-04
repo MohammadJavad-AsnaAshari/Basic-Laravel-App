6fd6ccf410e882e1e57ad15fa4c281406976ddfe
MohammadJavad-AsnaAshari <mohammadjavadasnaashari@gmail.com>
2023-09-04 12:54:46 +0330

    add Styling Post List Page

--- resources/views/posts/partials/post.blade.php
+++ resources/views/posts/partials/post.blade.php
@@ -1,16 +1,12 @@
-{{--<div>{{ $key }}. {{ $post["title"] }}</div>--}}
+<h3><a class="link-dark" style="text-decoration: none"
+       href="{{route("posts.show", ["post" => $post->id])}}">{{ $post->title }}</a></h3>
 
-@if($loop->even)
-    <div>{{ $post->title }}</div>
-@else
-    <div style="background-color: silver">{{ $post->title }}</div>
-@endif
-
-<div>
-    <form action="{{route("posts.destroy", ["post" => $post->id])}}" method="post">
+<div class="mb-3">
+    <a class="btn btn-primary" href="{{route("posts.edit", ["post" => $post->id])}}">Edit</a>
+    <form class="d-inline" action="{{route("posts.destroy", ["post" => $post->id])}}" method="post">
         @csrf
         @method("delete")
 
-        <input type="submit" value="Delete">
+        <input class="btn btn-danger" type="submit" value="Delete">
     </form>
 </div>
