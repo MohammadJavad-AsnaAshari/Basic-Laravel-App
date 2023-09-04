db07abe59db45f97cf8fbfde44aa080396ad2348
MohammadJavad-AsnaAshari <mohammadjavadasnaashari@gmail.com>
2023-09-04 13:15:58 +0330

    add Styling Single Post Page

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,25 +1,11 @@
 @extends("layouts.app")
-@section("title", $post["title"])
-
-@if($post["is_new"])
-    <div>A new blog post! Using if</div>
-@else
-    <div>Blog post is old! using elseif/else</div>
-@endif
-
-@unless($post["is_new"])
-    <div>It is an old post... using unless</div>
-@endunless
+@section("title", $post->title)
 
 @section("content")
-    <h1>{{$post["title"]}}</h1>
-    <p>{{$post["content"]}}</p>
-
-    @isset($post["has_comments"])
-        <div>The post has some comments... using isset</div>
-    @endisset
-
-    @empty($post["has_comments"])
-        <div>The post has some comments... using empty</div>
-    @endempty
+    <h1>{{$post->title}}</h1>
+    <p>{{$post->content}}</p>
+    <p>Added {{$post->created_at->diffForHumans()}}</p>
+    @if(now()->diffInMinutes($post->created_at) < 5)
+        <div class="alert alert-info">New!</div>
+    @endif
 @endsection
